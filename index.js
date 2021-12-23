6ca22267f76e09ef02d07d264dfaa93f8eee938b
samuel <Skboakye002@st.ug.edu.gh>
2021-12-23 06:37:14 +0000

    correcting alert

--- index.js
+++ index.js
@@ -5,73 +5,55 @@ let alertEl = document.getElementById("alert-el")
 let content = inputEl.value
  
 let tasks=[]
-
  
-
-    
-
 
  function buttonClick(){
      
 
     if(inputEl.value === ""){
-       
+     
     }
     else{
 
 
     tasks.push(inputEl.value) 
     renderTasks()
+    
+
     }
  }
  
 
 
-
-
-
-
 function renderTasks(){
 
-    listEl.innerHTML=" "
+    listEl.innerHTML=""
     for (i=0 ; i< tasks.length ; i++){
 
     
-        listEl.innerHTML += "<li>" + tasks[i] + " <button id='delete-el' onclick='deleteEl("+i+")'><i id= 'uji' class='fa fa-trash'></i></button></li> <br>"
-    
+        listEl.innerHTML += "<li>" + tasks[i] + "<button id='delete-el' onclick='deleteEl("+i+")'><i id= 'uji' class='fa fa-trash'></i></button></li> <br>"
         inputEl.value=null
-        let width = tasks.length 
         
-        
-         
-         alertEl.textContent = "You have" + " " + width + " " +"pending task(s)"
+        alertEl.textContent = "You have" + " " + tasks.length + " " +"pending task(s)"
+       
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 function deleteEl(i){
 
     tasks.splice(i , 1)
-    console.log(tasks)
+    
     listEl.innerHTML = " "
     
     renderTasks()
-     
+    console.log(tasks.length) 
+    alertEl.textContent = "You have" + " " + tasks.length + " " +"pending task(s)"
 }
 
  
 
-
-
+ 
 
 
 
