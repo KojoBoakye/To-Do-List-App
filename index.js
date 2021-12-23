69eec3a0ee75e55571719022d6812fcc955f57ce
samuel <Skboakye002@st.ug.edu.gh>
2021-12-23 04:33:17 +0000

    make delete buttons functional

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let inputEl = document.getElementById("input-el")
 let buttonEl = document.getElementById("button-el")
 let listEl = document.getElementById("list-el")
 let alertEl = document.getElementById("alert-el")
+ 
 let tasks=[]
 
  
@@ -9,24 +10,53 @@ let tasks=[]
     
 
 
-buttonEl.addEventListener("click" , function(){
+ function buttonClick(){
     listEl.innerHTML=" "
     let content = inputEl.value
     tasks.push(content) 
-    
- for (i=0 ; i< tasks.length ; i++){
-
-    
-    listEl.innerHTML += "<li>" + tasks[i] + " <button class='yapi' id='delete-el" + i + " ' onclick='deleteEl()'><i id= 'uji' class='fa fa-trash'></i></button></li>"
-
-    inputEl.value=null
-
-     alertEl.textContent = "You have" + " " + tasks.length + " " +"pending task(s)"
+    renderTasks()
+     
  }
  
 
-})
 
+
+
+
+
+function renderTasks(){
+
+
+    for (i=0 ; i< tasks.length ; i++){
+
+    
+        listEl.innerHTML += "<li>" + tasks[i] + " <button id='delete-el' onclick='deleteEl("+i+")'><i id= 'uji' class='fa fa-trash'></i></button></li>"
+    
+        inputEl.value=null
+         
+
+         alertEl.textContent = "You have" + " " + tasks.length + " " +"pending task(s)"
+    
+}
+}
+
+
+
+
+
+
+
+
+
+function deleteEl(i){
+
+    tasks.splice(i , 1)
+    listEl.innerHTML = " "
+    renderTasks()
+     
+}
+
+ 
 
 
 
